4c7b10154f200f4483da8db39d13ef5515686dbf
Avocirc <crist@Avocirc>
2023-04-17 02:09:19 +0300

    Half part of adaptive

--- js/main.js
+++ js/main.js
@@ -32,6 +32,13 @@ function open(evt) {
 }
 
 
+const menuBtn = document.querySelector('.menu__btn')
+const menu = document.querySelector('.menu__list')
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('menu__list--active')
+})
+
 const swiper = new Swiper(".swiper", {
   effect: "fade",
   pagination: {
